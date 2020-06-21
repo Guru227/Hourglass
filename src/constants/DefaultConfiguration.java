b2f35a8c489ae1f8e102e20771c94a7a4ad753a5
Gurusankar K <narain2000@gmail.com>
2020-06-21 22:34:57 +0530

    Redundant configuration fields removed.

--- src/constants/DefaultConfiguration.java
+++ src/constants/DefaultConfiguration.java
@@ -1,8 +1,6 @@
 package constants;
 
 public final class DefaultConfiguration {
-	public static final int screenDisableDuration = 15;
-	public static final int timerDuration = 30;
 	public static final boolean fadeIn = false;
 	public static final boolean slideIn = false;
 	public static final boolean darkMode = false;
